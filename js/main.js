e60eff68e977808e960081d61e320eab0ab33907
mage1987 <michal.hanzal@gmail.com>
2018-03-20 15:59:09 +0100

    1.0a
    První pokusy a polofunkcni verze.

---
+++ js/main.js
@@ -0,0 +1,149 @@
+// TODO ﻿
+// tlacitko na klic ze vsech smeru
+// dynamicky text klici na komode
+
+var progress = 0;
+var inventory = 0;
+var pozice = 0;
+
+
+function displayDynButton(elmID) {
+  document.getElementById(elmID).style.display="block";
+}
+
+function hideDynButton(elmID) {
+  document.getElementById(elmID).style.display="none";
+}
+
+function Room(name, left, right, ahead, msgLeft, msgAhead, msgRight, msgBack) {
+	this.name = name;
+	this.left = left;
+	this.right = right;
+	this.ahead = ahead;
+  this.msgLeft = msgLeft;
+  this.msgAhead = msgAhead;
+  this.msgRight = msgRight;
+  this.msgBack = msgBack;
+  this.observe = function() {
+    // pozice = 0 - jsem na začátku
+    if (pozice == 0) {
+      return "Před sebou: " + this.ahead + ", vlevo:  " + this.left + ", vpravo: " + this.right;
+    // pozice = 1 - jsem vlevo
+    } else if (pozice == 1) {
+      return "Před sebou: " + this.right + ", vlevo: " + this.ahead;
+    // pozice = 2 - jsem vepředu
+    } else if (pozice == 2) {
+      return "Vlevo: " + this.right + ", vpravo: " + this.left;
+    } else {
+      return "Před sebou: " + this.left + ", vpravo: " + this.ahead;
+    }
+  };
+  this.goBack = function () {
+    pozice = 0;
+    return this.msgBack;
+  };
+  this.goLeft = function () {
+    if (pozice == 0) {
+      pozice = 1;
+      return this.msgLeft;
+    } else if (pozice == 1) {
+      pozice = 2;
+      return this.msgAhead;
+    } else if (pozice == 2) {
+      pozice = 3;
+      return this.msgRight;
+    } else {
+      pozice = 0;
+      return this.msgBack;
+    }
+  };
+  this.goAhead = function () {
+    if (pozice == 0) {
+      pozice = 2;
+      return this.msgAhead;
+    } else if (pozice == 1) {
+      pozice = 3;
+      return this.msgRight;
+    } else if (pozice == 2) {
+      pozice = 0;
+      return this.msgBack;
+    } else {
+      pozice = 1;
+      return this.msgLeft;
+    }
+  };
+  this.goRight = function () {
+    hideDynButton("dynamicButton");
+    if (pozice == 0) {
+      pozice = 3;
+      return this.msgRight;
+    } else if (pozice == 1) {
+      pozice = 0;
+      return this.msgBack;
+    } else if (pozice == 2) {
+      pozice = 1;
+      return this.msgLeft;
+    } else {
+      pozice = 2;
+      return this.msgAhead;
+    }
+  };
+  this.pickUp = function () {
+    inventory = 1;
+    document.getElementById("dynamicButton").style.display="none";
+    return "Sebral jsi klíč."
+  };
+}
+
+function Item(name) {
+  this.name = name;
+  this.takeItem = function() {
+    inventory = 1;
+    return "Sebral jsi " + this.name + ".";
+  };
+  this.useItem = function() {
+    inventory = 0;
+    return "Použil jsi " + this.name + ".";
+  };
+};
+
+var darkRoom = new Room(
+  "Dark Room",
+  "komoda",
+  "okno",
+  "dveře",
+  "Jsi u komody.",
+  "Jsi u dveří.",
+  "Jsi u okna.",
+  "Jsi zpět na začátku."
+);
+darkRoom.interact = function() {
+  if (pozice == 1) {
+    return "Na komodě leží klíč.";
+  } else if (pozice == 2) {
+    return "Dveře jsou zamčené, asi to bude chtít klíč.";
+  } else if (pozice == 3) {
+    return "Venku je tma a prší.";
+  } else {
+    return "No jsi v pokoji a potřebuješ ven. Kapiš to?!";
+  };
+};
+
+var lightRoom = new Room(
+  "Light Room",
+  "postel",
+  "záchod",
+  "prasklá zeď",
+  "Jsi u postele.",
+  "Jsi u záchodu.",
+  "Jsi u prasklé zdi.",
+  "Jsi zpět na začátku."
+);
+
+var klic = new Item("klíč");
+
+
+function headerRoomSetter() {
+  progress = 1;
+  return "Pokoj číslo " + progress + ", '" + darkRoom.name + "'.";
+};
