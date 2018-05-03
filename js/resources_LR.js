86dcb981eab129e2a8c53d2a5e0f36f2b2fc2aa1
mage1987 <michal.hanzal@gmail.com>
2018-05-03 21:35:04 +0200

    room2 done

--- js/resources_LR.js
+++ js/resources_LR.js
@@ -2,17 +2,17 @@ var v_actualUser = [
   "Michael"
 ];
 
-var v_initialMsg = "\"Oh, hi Michael. So you found the key I've hidden in the cupboard. What a skilful and clever boy.\" <br> There was a short silence and you thought The Voice kinda smiled and blinked, or something.<br> \"So let's play some more. What you say? It was pretty easy with those prepared buttons and all, right? Let's make it little harder. I'm giving you The Command Line. Yeah man, that field right in front of you. Enjoy, dude!<br>What da hell? You thougth. Somebody is shiting me, right? What am I supposed to do with that? Am I in a computer game? You remembered one of your first computers and the crude UI it had back then. Or better to say it didn't have. You looked around yourself. There is literally nothing around you, just white everywhere, endless. And obviously, The Command Line floating in the midair with big Execute! button and something like keyboard.<br>Alright, let's try it. What was the command to get some help in that ancient computer of yours...? \" "; // nápis na první obrazovce, welcome pokoje
+var v_initialMsg = "\"Oh, hi Michael. So you found the key I've hidden in the cupboard. What a skilful and clever boy.\" <br> There was a short silence and you thought The Voice kinda smiled and blinked, or something like that.<br> \"So let's play some more. What you say? It was pretty easy with those prepared buttons and all, right? Let's make it little harder. I'm giving you The Command Line. Yeah man, that field right in front of you. Enjoy, dude!\"<br><br>What da hell? You thougth. Somebody is shiting me, right? What am I supposed to do with that? Am I in a computer game? You remembered one of your first computers and the crude UI it had back then. Or better to say it didn't have. You looked around yourself. There is literally nothing around you, just white everywhere, endless. And obviously, The Command Line floating in the midair with big Execute! button and something like keyboard.<br>Alright, let's try it. What was the command to get some help in that ancient computer of yours...? \" "; // nápis na první obrazovce, welcome pokoje
 
 var v_actualRoom = [
-  "The Bright Room",   // jméno pokoje
+  "The White Room",   // jméno pokoje
   2     // ID pokoje
 ];
 
 var v_commands = [
   ": command not found",
-  "Lookin'for help, eh? Let's play a game of riddles. The first one goes:<div style=\"text-align: center;\"><br><br>You must prove yourself worthy, friend<br>As the fellowship at The Mine<br>It's just one word that will take you to the end<br>If you know what's on my mind.</div>",
-  "Yes. \"Friend\", \"Mellon\" in Quenya, is the word that opened the Mines of Moria when Gandalf said it. Now, here goes another riddle:<div style=\"text-align: center;\"><br><br>There are many of us more than sixtyfour. Do you know us?<br>Can you guess our name?<br><br>V2UgYXJlIHRoZSBMZWdpb24u</div>",
-  "You are good as hell. And I'm tired of making up riddles. So, here is the last one:<br>What is the name of the monster of monsters made by the prince of horror?<br>When you get it right, just exit The Command Line and you can leave. I'm bored... Fuck of!",
+  "Lookin'for help, eh? Let's play a game of riddles. An easy one for the start. The first one goes:<div style=\"text-align: center;\"><br><br>You must prove yourself worthy, friend<br>As the fellowship at The Mine<br>It's just one word that will take you to the end<br>If you know what's on my mind.</div>",
+  "Yes. \"Friend\", \"Mellon\" in Quenya, is the word that opened the Mines of Moria when Gandalf said it. Now, here goes another riddle:<div style=\"text-align: center;\"><br><br>I, we are a crazy bunch, there is many of me, of us. Exactly 5:9.<br>What is my name?<div style=\"color: white;\">TXkgbmFtZSBpcyBMZWdpb24sIGZvciB3ZSBhcmUgbWFueS4=</div></div>",
+  "You are good as hell. And I'm tired of making up riddles. So, here is the last one:<div style=\"text-align: center;\"><br>There is a monster of monsters created by the prince of horror? It creeps in the darkness, terifying. What is it?</div><br>When you get it right, just exit The Command Line and you can leave. I'm bored... Fuck off!",
   "Bingo! You got it! The command line dissapeared and a big button with a EXIT label poped-up in front of you."
 ];
